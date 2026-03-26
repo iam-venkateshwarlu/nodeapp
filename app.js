151bed41aba4839ca57d3dedf30d2d172a14c430
venkateshwarlu <59246422+iam-venkateshwarlu@users.noreply.github.com>
2026-03-26 01:04:38 +0000

    Update welcome message in Express app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express App');
+  res.send('Hello Welcome Express App ');
 });
 
 app.listen(3000, () => {
